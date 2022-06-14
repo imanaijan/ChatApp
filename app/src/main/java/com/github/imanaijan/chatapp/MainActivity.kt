ddddbd89a9cf7fe3013bcdbb54c23e33356d3638
Ruslan Imanaev <imanaijann@gmail.com>
2022-06-14 13:56:15 +0300

    Initial commit

---
+++ app/src/main/java/com/github/imanaijan/chatapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.github.imanaijan.chatapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
