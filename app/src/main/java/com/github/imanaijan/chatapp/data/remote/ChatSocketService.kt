6b8f30d448d6ac7c966255381e2e8095d525a55b
Ruslan Imanaev <imanaijann@gmail.com>
2022-06-15 19:35:32 +0300

    add ChatSocketService

---
+++ app/src/main/java/com/github/imanaijan/chatapp/data/remote/ChatSocketService.kt
@@ -0,0 +1,26 @@
+package com.github.imanaijan.chatapp.data.remote
+
+import com.github.imanaijan.chatapp.domain.model.Message
+import com.github.imanaijan.chatapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface ChatSocketService {
+
+    suspend fun initSession(
+        username: String
+    ): Resource<Unit>
+
+    suspend fun sendMessage(message: String)
+
+    fun observerMessages(): Flow<Message>
+
+    suspend fun closeSession()
+
+    companion object {
+        const val BASE_URL = "ws://192.168.1.104:8080"
+    }
+
+    sealed class Endpoints(val url: String) {
+        object ChatSocket: Endpoints("$BASE_URL/chat-socket")
+    }
+}
