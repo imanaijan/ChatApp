7f6c1035a378438ce1270958f9d914e7cc57442e
Ruslan Imanaev <imanaijann@gmail.com>
2022-06-16 19:38:50 +0300

    Navigation setup is done

--- app/src/main/java/com/github/imanaijan/chatapp/MainActivity.kt
+++ app/src/main/java/com/github/imanaijan/chatapp/MainActivity.kt
@@ -3,12 +3,41 @@ package com.github.imanaijan.chatapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.github.imanaijan.chatapp.presentation.chat.ChatScreen
+import com.github.imanaijan.chatapp.presentation.username.UsernameScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "username_screen"
+            ) {
+                composable("username_screen") {
+                    UsernameScreen(onNavigate = navController::navigate)
+                }
+                composable(
+                    route = "chat_screen/{username}",
+                    arguments = listOf(
+                        navArgument(name = "username") {
+                            type = NavType.StringType
+                            nullable = true
+                        }
+                    )
+                ) {
+                    val username = it.arguments?.getString("username")
+                    ChatScreen(username = username)
+                }
+            }
         }
     }
 }
